13183d4340eaaa46a0892e46667ff02aaaef38a1
PercyGFX <isurangabtk@gmail.com>
2024-11-19 00:04:14 +0530

    multer

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,12 +1,31 @@
 import express from "express";
 const productRoutes = express.Router();
+import multer from "multer";
+
 import {
   getAllproducts,
   Addproduct,
 } from "../controllers/productController.js";
 
+// multer configuration
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 // add new product
-productRoutes.post("/add-new", Addproduct);
+productRoutes.post(
+  "/add-new",
+  upload.fields([
+    { name: "mainImage", maxCount: 1 },
+    { name: "otherImages", maxCount: 5 },
+  ]),
+  Addproduct
+);
 
 // get all products
 
